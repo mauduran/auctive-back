a665e8f145dc1ae6ed8c6af111d89521917dc5fb
mauduran <is716122@iteso.mx>
2021-04-01 21:27:34 -0600

    Added function definitions for user utils

--- src/utils/user.utils.js
+++ src/utils/user.utils.js
@@ -1,1 +1,31 @@
-// TODO: Add function definitions
+const signToken = (email) => {
+    return jwt.sign({
+        email
+    }, process.env.TOKEN_SECRET);
+}
+const findToken = async (token) => { }
+
+const findUsers = async (query) => { }
+
+const findUserByEmail = async (email) => { }
+
+const findUserById = async (userId) => { }
+
+const deleteUser = async (userId) => { }
+
+const changePassword = async (userId, currentPassword, newPassword) => { }
+
+const updateUserWithGoogleId = async (userId, googleId) => { }
+
+const updateUserProfilePic = async (userId, imageUrl) => { }
+
+module.exports = {
+    findToken,
+    findUsers,
+    findUserByEmail,
+    findUserById,
+    deleteUser,
+    changePassword,
+    updateUserWithGoogleId,
+    updateUserProfilePic
+}
