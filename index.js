34447d2049dbea1a5246c09d4c0d2daed4b65f9a
mauduran <is716122@iteso.mx>
2021-04-01 21:21:01 -0600

    Added routes to index + included more utils definitions

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 
+const socketConfig = require('./src/utils/socket.utils');
+
+const notificationsRoute = require('./src/routes/notifications.route');
+const auctionsRoute = require('./src/routes/auctions.route');
+const userRoute = require('./src/routes/user.route');
+
 if(process.env.NODE_ENV=='dev'){
     require('dotenv').config();
 }
@@ -14,9 +20,10 @@ app.use(express.urlencoded({extended: true}));
 
 app.use('/assets', express.static(path.join(__dirname, 'public')));
 
+app.use('/api/notifications', notificationsRoute);
+app.use('/api/auctions', auctionsRoute);
+app.use('/api/user', userRoute);
 
-// const socketUtils = require('./src/utils/socket-dictionary.utils');
-// const socketConfig = require('./src/utils/socket.utils');
 
 const app = express();
 
---
+++ src/utils/auction.utils.js
@@ -0,0 +1,23 @@
+const createAuction = async (auction) => {}
+
+const findAuctions = async (query) => {}
+
+const findAuctionById = async (auctionId) => {}
+
+const findAuctionsByCategory = async (category, query) => {}
+
+const updateAuctionWithBid = async (auctionId, bid) => {}
+
+const deleteAuction = async (auctionId) => {}
+
+const updateAuction = async (auctionId, auction) => {}
+
+module.exports = {
+    createAuction,
+    findAuctions,
+    findAuctionById,
+    findAuctionsByCategory,
+    updateAuctionWithBid,
+    deleteAuction, 
+    updateAuction
+}
