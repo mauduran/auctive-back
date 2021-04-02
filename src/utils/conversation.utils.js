5e45d67b2212ec564976ffccd10c0b3260a77aa1
mauduran <is716122@iteso.mx>
2021-04-01 21:48:32 -0600

    Added function definitions for conversations utils

---
+++ src/utils/conversation.utils.js
@@ -0,0 +1,16 @@
+
+const createConversation = async (auctionId, user, auctionOwner) => { }
+
+const leaveConversation= async (conversationId, userId) => { }
+
+const addMessageToConversation = async (conversationId, message) => {}
+
+const deleteMessageFromConversation = async (conversationId, messageId) => {}
+
+
+module.exports = {
+    createConversation,
+    leaveConversation,
+    addMessageToConversation,
+    deleteMessageFromConversation
+}
