165426644e0e6fc69d8576bad6cd82fe7a76c965
mauduran <is716122@iteso.mx>
2021-04-01 20:55:06 -0600

    Added auctions route and controller definitions

---
+++ src/controllers/auctions.controller.js
@@ -0,0 +1,18 @@
+
+const getAuctions = async (req, res) => { }
+
+const getAuctionsByCategory = async (req, res) => { }
+
+const getAuctionById = async (req, res) => { }
+
+const removeAuctionById = async (req, res) => { }
+
+const createAuction = async (req, res) => {}
+
+module.exports = {
+    getAuctions,
+    getAuctionsByCategory,
+    getAuctionById,
+    removeAuctionById,
+    createAuction
+}
---
+++ src/routes/auctions.route.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const auctionsController = require('../controllers/auctions.controller');
+const authMiddleware = require("../middlewares/auth.middleware");
+
+const router = express.Router();
+
+router.route('/')
+    .get(auctionsController.getAuctions)
+    .post(authMiddleware, auctionsController.createAuction);
+
+router.route('/category/:category')
+    .get(auctionsController.getAuctionsByCategory);
+
+router.route('/:id')
+    .get(auctionsController.getAuctionById)
+    .delete(auctionsController.removeAuctionById)
