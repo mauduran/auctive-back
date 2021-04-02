c88a5aa045953d12c9cd12b58461c7ace954fc7e
mauduran <is716122@iteso.mx>
2021-04-01 20:27:34 -0600

    Added notification utils function definitions

---
+++ src/utils/notification.utils.js
@@ -0,0 +1,11 @@
+const generateNotification = async (userId, message, file, emitter) => {}
+
+const deleteNotification = async(userId, notificationId) =>{}
+
+const deleteAllNotifications = async(userId) => {}
+
+module.exports = {
+    generateNotification,
+    deleteNotification,
+    deleteAllNotifications
+}
