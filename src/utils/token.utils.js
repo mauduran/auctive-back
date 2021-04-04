618c42efec77dff8392dfdaa9327b2bd9ea155bf
mauduran <is716122@iteso.mx>
2021-04-04 00:26:53 -0600

    Added functions to sign tokens and get user from token

--- src/utils/token.utils.js
+++ src/utils/token.utils.js
@@ -1,9 +1,32 @@
 const jwt = require("jsonwebtoken");
 
+const { dynamoDB } = require("../../config/aws.config")
+
 const signToken = (email) => {
-    return jwt.sign({
+    const token = jwt.sign({
         email
     }, process.env.TOKEN_SECRET);
+
+    params = {
+        TableName: process.env.AWS_DYNAMODB_TABLE,
+        Key: {
+            "PK": `USER#${email}`,
+            "SK": `#PROFILE#${email}`
+        },
+        UpdateExpression: "set session_token = :session_token",
+        ExpressionAttributeValues: {
+            ":session_token": token,
+        },
+        ReturnValues: "UPDATED_NEW"
+    }
+
+    return dynamoDB.update(params).promise()
+        .then(user => {
+            return token;
+        })
+        .catch(err => {
+            return err;
+        });
 }
 
 const verifyToken = (token) => {
@@ -11,6 +34,36 @@ const verifyToken = (token) => {
 }
 
 const saveToken = async (userId, token) => { }
+
+const getUserFromToken = async (token) => {
+    params = {
+        TableName: process.env.AWS_DYNAMODB_TABLE,
+        IndexName: process.env.AWS_DYNAMODB_SESSION_INDEX,
+        KeyConditionExpression: "session_token = :token",
+        ExpressionAttributeValues: {
+            ":token": token,
+        },
+    }
+    try {
+        let data = await dynamoDB.query(params).promise();
+
+        if (!data.Items || !data.Items.length) {
+            throw "Could not find token";
+        }
+        user = data.Items[0];
+
+        const verification = jwt.verify(user.session_token, process.env.TOKEN_SECRET);
+        if (verification) {
+            delete user.hash;
+            return Promise.resolve(user);
+        }
+        throw "Token is not valid";
+
+    } catch (error) {
+        return Promise.reject(error);
+    }
+
+}
 
 const removeToken = async (tokenId) => { }
 
@@ -23,4 +76,5 @@ module.exports = {
     saveToken,
     findToken,
     removeToken,
+    getUserFromToken
 }
