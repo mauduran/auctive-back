3641eefbd520d590ddec038b244ee25e3ec770e4
mauduran <is716122@iteso.mx>
2021-04-07 22:03:38 -0500

    Added functionality to certain functions in conversation.utils

--- src/utils/conversation.utils.js
+++ src/utils/conversation.utils.js
@@ -1,33 +1,91 @@
+const { dynamoDB } = require("../../config/aws.config")
+
+if (process.env.NODE_ENV == 'dev') {
+    require('dotenv').config();
+}
+
 
 const createConversation = async (auctionId, user, auctionOwner) => {
 
 }
 
-const leaveConversation = async (conversationId, userId) => {
+const leaveConversation = async (conversationId, userEmail) => {
 
 }
 
-//TODO: Traer código del otro proyecto
-const getConversationById = async (conversationId, userId) => {
+
+const addMessageToConversation = async (conversationId, message, senderEmail, senderName) => {
 
 }
 
-//TODO: Traer código del otro proyecto
+//TODO: Test functionality and consider including "#CONVERSATION#" in the conversationId
+const getConversationById = async (conversationId, userEmail) => {
+    //Validate user is in conversation with filter query
+    params = {
+        TableName: process.env.AWS_DYNAMODB_TABLE,
+        IndexName: 'reverse-index',
+        KeyConditionExpression: "SK = :sk  and begins_with (PK, :pk)",
+        ExpressionAttributeValues: {
+            ":pk": `AUCTION#`,
+            ":sk": `#CONVERSATION#${conversationId}`
+        },
+    }
+    return dynamoDB.query(params).promise()
+        .then(data => {
+            if (data.Items.length) return data.Items[0];
+            throw "Could not find conversation"
+        });
+}
+
+//TODO: Test functionality
 const getConversations = async (email) => {
+    params_1 = {
+        TableName: process.env.AWS_DYNAMODB_TABLE,
+        IndexName: 'seller_email-index',
+        KeyConditionExpression: "seller_email = :seller_email",
+        ExpressionAttributeValues: {
+            ":seller_email": email
+        },
+    }
 
+    params_2 = {
+        TableName: process.env.AWS_DYNAMODB_TABLE,
+        IndexName: 'bidder_email-index',
+        KeyConditionExpression: "bidder_email = :bidder_email",
+        ExpressionAttributeValues: {
+            ":bidder_email": email
+        },
+    }
+
+    let conversations = [];
+
+    return dynamoDB.query(params_1).promise()
+        .then(data => {
+            conversations.push(...data.Items);
+            return dynamoDB.query(params_2).promise()
+        })
+        .then(data => {
+            conversations.push(...data.Items);
+            return conversations;
+        });
 }
 
-
-const addMessageToConversation = async (conversationId, message) => {
-    
-}
-
-//TODO: Traer código del otro proyecto
+//TODO: Test functionality and consider including "#CONVERSATION#" in the conversationId
 const getMessagesFromConversation = async (conversationId) => {
-
+    params = {
+        TableName: process.env.AWS_DYNAMODB_TABLE,
+        KeyConditionExpression: "PK = :pk  and begins_with (SK, :sk)",
+        ExpressionAttributeValues: {
+            ":pk": `#CONVERSATION#${id}`,
+            ":sk": '#MESSAGE#'
+        },
+    }
+    return dynamoDB.query(params).promise()
+        .then(data => data.Items);
 }
 
-const deleteMessageFromConversation = async (conversationId, messageId) => {
+//TODO: Borrado Lógico. Los usuarios verán "Mensaje eliminado"
+const deleteMessageFromConversation = async (conversationId, messageId, userEmail) => {
 
 }
 
