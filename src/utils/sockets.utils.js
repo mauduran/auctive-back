3625871b4f5d5b8ab1b1e27a38ae7a548c7d70ad
mauduran <is716122@iteso.mx>
2021-04-07 21:49:02 -0500

    Added comments to socket utils

--- src/utils/sockets.utils.js
+++ src/utils/sockets.utils.js
@@ -1,9 +1,10 @@
 const socketIo = require('socket.io');
 const socketUtils = require('./socket-dictionary.utils');
 
+let io;
 const socketInit = (server) => {
 
-    const io = socketIo(server, {
+    io = socketIo(server, {
         cors: {
             origin: '*',
             methods: ['GET', 'POST'],
@@ -18,31 +19,88 @@ const socketInit = (server) => {
 
         socketUtils.addActiveUser(socket.id, userId);
 
-        console.log('Client connected', socket.id);
+        // TODO: Check is user is Admin and join admin room.
+
+        //TODO: if not user, join all auction rooms
 
         socket.on('disconnect', () => {
             socketUtils.removeActiveUser(userId);
             console.log('Client disconnected');
         });
 
-        socket.on('notification', async data => { })
+        /*
+        TYPES OF NOTIFICATION
+         Admin - New verification request
+         User - Change on verification status.
+         User - New conversation
+         User - New Bid on Auction
+         Owner - New Bid on Auction
+         User - Buy now event
+         Owner - Buy now event
+         Owner - End of auction (Expired, Closed)
+         User - End of auction
+         AuctionWinner - End of auction (bid won)
+        */
+        socket.on('verify', async data => {
+            //Update dynamodb document.
 
-        socket.on('message', async data => { });
+            //SEND NOTIFICATION TO USER with new status and message.
+        });
 
-        socket.on('deleteMessage', async data => { });
+        socket.on('verificationRequest', async data => {
+            //Add image to S3 bucket.
+            //Add or update dynamodb document.
+            //Remove old image from S3 bucket if any
+            //Emit to admin room
 
-        socket.on('createConversation', async data => { });
+        });
 
-        socket.on('removeConversation', async data => { });
+        socket.on('message', async data => {
+            //Add message document to dynamoDB
+            //Emit message to user 
+        });
 
-        socket.on('verificationRequest', async data => { });
+        socket.on('deleteMessage', async data => {
+            //Update message document to dynamoDB
+            //Emit message to user
+        });
 
-        socket.on('newBid', async data => { });
+        socket.on('createConversation', async data => {
+            //Create conversation Document.
+            //SEND NOTIFICATION TO USER new conversation
+            //Emit notification to user.
+        });
 
-        socket.on('buyNow', async data => { });
+        socket.on('removeConversation', async data => { 
+            //Update conversation document (remove seller or bidder)
+            //SEND message to other USER 'user has left conversation'
+        });
+
+
+        socket.on('subscribeToAuction', async data => { 
+            //Update conversation document (Add user to Array if not there already)
+            //Join auction room
+        });
+
+        socket.on('newBid', async data => {
+            //Update conversation document with new bid and email of biddder
+            //SEND NOTIFICATION TO OWNER AND INTERESTED PEOPLE
+        });
+
+        socket.on('buyNow', async data => {
+            //Update conversation document with bidWinner and change status to closed
+            //SEND NOTIFICATION TO OWNER AND INTERESTED PEOPLE
+        });
     });
 }
 
+notifyEndOfAuction = (auction) => {
+    //Check if auction was closed or expired (Notify owner)
+    //Notify bid winner if one exists
+    //Notify all interested parties that auction has ended.
+}
+
 module.exports = {
-    socketInit
+    socketInit,
+    notifyEndOfAuction
 }
