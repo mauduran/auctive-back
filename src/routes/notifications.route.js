672fe0781c5e530e0f9d6f3f5335c4cdac93aa2f
Edgar Medina <is715468@iteso.mx>
2021-04-10 18:48:51 -0500

    added new controller in "/" endpoint

--- src/routes/notifications.route.js
+++ src/routes/notifications.route.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.route('/')
     .get(authMiddleware, notificationsController.getNotifications)
+    .post(authMiddleware, notificationsController.createNotification)
     .delete(authMiddleware, notificationsController.deleteAllNotifications);
 
 router.route('/:id')
