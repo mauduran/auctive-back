4ac9d64416b121ab931d16ef31baae1352cbc840
Edgar Medina <is715468@iteso.mx>
2021-04-13 17:52:12 -0500

    added createConversation function

--- src/utils/conversation.utils.js
+++ src/utils/conversation.utils.js
@@ -1,12 +1,40 @@
 const { dynamoDB } = require("../../config/aws.config")
+const { v4: uuidv4 } = require('uuid');
 
 if (process.env.NODE_ENV == 'dev') {
     require('dotenv').config();
 }
 
 //TODO: Create conversation, only the bidder can start the conversation. Last message is empty
-const createConversation = async (auctionId, user, auctionOwner) => {
+const createConversation = async (auctionId, bidderEmail, auctionOwnerEmail) => {
+    let conversationId = uuidv4();
 
+    let conversation = {
+        PK: `AUCTION#${auctionId}`,
+        SK: `#CONVERSATION#${conversationId}`,
+        auctionId: auctionId,
+        bidder_email: bidderEmail,
+        last_message_date: new Date().toDateString(),
+        last_message_sender: "",
+        last_message_value: "",
+        seller_email: auctionOwnerEmail,
+    }
+
+    params = {
+        TableName: process.env.AWS_DYNAMODB_TABLE,
+        ConditionExpression: "attribute_not_exists(PK)",
+        Item: conversation
+    }
+
+    return new Promise((resolve, reject) => {
+        dynamoDB.put(params, (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    });
 }
 
 //TODO: Update conversation and remove user from it. Does not remove the conversation
@@ -30,7 +58,4 @@ module.exports = {
     leaveConversation,
     addMessageToConversation,
     deleteMessageFromConversation,
-    getMessagesFromConversation,
-    getConversationById,
-    getConversations
 }
