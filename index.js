1b56f377435a3810f890cfd9d5a73ca387500731
Edgar Medina <is715468@iteso.mx>
2021-04-13 17:50:13 -0500

    added use of conversationRoute in index

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const socketConfig = require('./src/utils/sockets.utils');
 const notificationsRoute = require('./src/routes/notifications.route');
 const auctionsRoute = require('./src/routes/auctions.route');
 const userRoute = require('./src/routes/user.route');
+const conversationRoute = require('./src/routes/conversations.route');
 const scheduleUtils = require('./src/utils/auction-schedule.utils');
 if (process.env.NODE_ENV == 'dev') {
     require('dotenv').config();
@@ -26,6 +27,7 @@ app.use('/assets', express.static(path.join(__dirname, 'public')));
 app.use('/api/auctions', auctionsRoute);
 app.use('/api/user/notifications', notificationsRoute);
 app.use('/api/users', userRoute);
+app.use('/api/conversations', conversationRoute);
 
 app.use('*', (req, res) => {
     res.status(404).json({ error: true, message: "Endpoint not found" });
